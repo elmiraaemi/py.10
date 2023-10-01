13411e43ca511a1014974cc3816c4096af23988e
elmiraaemi <108354246+elmiraaemi@users.noreply.github.com>
2023-10-01 21:14:33 +0330

    Add files via upload

---
+++ Date.py
@@ -0,0 +1,15 @@
+class Date:
+    #properties
+    def __init__(self, d, m, y):
+        self.day = d
+        self.month = m
+        self.year = y
+    #methods
+    def miladi_to_shamsi(self):
+        ...
+    def shamsi_to_miladi(self):
+        ...
+    def today_date(self):
+        ...
+    def calculate_age(self):
+        ...
---
+++ time.py
@@ -0,0 +1,15 @@
+class Time:
+    #properties
+    def __init__(self, h, m, s):
+        self.hour = h
+        self.minute = m
+        self.second = s
+    #methods
+    def time_to_second(self):
+        pass
+    def second_to_time(self):
+        pass
+    def stopwatch(self):
+        pass
+    def Timer(self):
+        pass
